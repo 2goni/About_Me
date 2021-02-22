67e2966eeea6b39e872957f569b0cc76b1e0dd62
2goni <gh2684@naver.com>
2021-02-22 13:49:13 +0900

    Update

---
+++ contents/js/favorite.js
@@ -0,0 +1,32 @@
+$(function(){
+    $('.fish').mouseenter(function(){
+      $('.show').text('고등어 구이를 좋아함');
+    });
+    $('.fish').mouseleave(function(){
+        $('.show').text('');
+    });
+});
+$(function(){
+    $('.game').mouseenter(function(){
+      $('.show').text('게임을 좋아함');
+    });
+    $('.game').mouseleave(function(){
+        $('.show').text('');
+    });
+});
+$(function(){
+    $('.baseball').mouseenter(function(){
+      $('.show').text('야구를 좋아함');
+    });
+    $('.baseball').mouseleave(function(){
+        $('.show').text('');
+    });
+});
+$(function(){
+    $('.travle').mouseenter(function(){
+      $('.show').text('여행을 좋아함');
+    });
+    $('.travle').mouseleave(function(){
+        $('.show').text('');
+    });
+});
